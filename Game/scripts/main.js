bc19f365fdde416a2f2b6b690ea4bded0065bf00
Bastien Germain <bastiengermain1708@hotmail.fr>
2019-01-15 11:23:54 +0100

    test tone with data

--- Game/scripts/main.js
+++ Game/scripts/main.js
@@ -4,6 +4,28 @@
 * See data structure in dataProcess.js
 */
 
+data = getData();
+
 function printData() {
-    console.log(getData());
+    console.log(data);
+}
+
+// ***  Exemple using tone  *** /
+
+var synth = new Tone.Synth().toMaster();
+
+//set the transport to repeat
+Tone.Transport.loopEnd = '1m'; // 1 measure is the loop's length
+Tone.Transport.loop = true;
+
+//start/stop the transport
+Tone.Transport.start();
+//Tone.Transport.stop()
+
+function playSound() {
+    if (data["lastPlayer"] == "Black") {
+        synth.triggerAttackRelease('C3', '4n');
+    } else {
+        synth.triggerAttackRelease('C4', '4n');
+    }
 }
