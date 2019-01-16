7ccad9ef9ee2a5dc6e0c2dd8b26faff557ec9b39
Bastien Germain <bastiengermain1708@hotmail.fr>
2019-01-16 11:03:17 +0100

    add captured stones data

--- Game/scripts/dataProcess.js
+++ Game/scripts/dataProcess.js
@@ -10,6 +10,8 @@ data["lastStonePosition"] = [0, 0]; // Range [0, 18]; 0,0 is top-left of goban
 data["stonesAround"] = 0; // number of stones around the last played stone
 data["whiteStonesAround"] = 0; // same with only white
 data["blackStonesAround"] = 0; // same with only black
+data["whiteCaptures"] = 0; // number of black stones captured by white
+data["blackCaptures"] = 0;
 
 
 var boardMat = math.zeros(19, 19);
@@ -31,16 +33,20 @@ function beginSGF(file) {
     //console.log(boardMat);
 
 	document.querySelector('#addMove').addEventListener('mousedown', function(e) {
+        move(1);
 		boardMat = fillMatrix(boardMat, sgf, moveNumber);
         data = getLastPlayer(data, sgf, moveNumber);
         data = getLastStonePosition(data, sgf, moveNumber);
         data = getStonesAround(data, boardMat);
         data["stoneOnBoard"] += 1;
+        data["whiteCaptures"] = getInfo().captures[JGO.WHITE];
+        data["blackCaptures"] = getInfo().captures[JGO.BLACK];
 		moveNumber++;
 	})
 
     // To go back one move
     document.querySelector('#removeMove').addEventListener('click', function(e) {
+        move(-1);
         moveNumber--;
 		boardMat = emptyMatrix(boardMat, sgf, moveNumber);
         data = getLastPlayer(data, sgf, moveNumber-1);
@@ -82,8 +88,8 @@ function getStonesAround(dataObject, boardMat) {
 
     var i, j;
 
-    for (x == 0 ? i = 0 : i = x - 1; i <= x + 1; i++) {
-        for (y == 0 ? j = 0 : j = y - 1; j <= y + 1; j++) {
+    for (x == 0 ? i = 0 : i = x - 1; i <= (x == 18 ? x : x + 1); i++) {
+        for (y == 0 ? j = 0 : j = y - 1; j <= (y == 18 ? y : y + 1); j++) {
             if (i != x || j != y) {
 
                 var matValue = math.subset(boardMat, math.index(j, i));
